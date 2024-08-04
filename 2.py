3b685f065f1924aaa0307f3fc5a4c6c80460ae85
helexrox <ankithtech2003@gmail.com>
2024-08-05 04:49:03 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,4 +1,3 @@
-#Program: For a given set of training data examples stored in a .CSV file, implement anddemonstrate the Candidate-Elimination algorithm to output a description of the set of all hypotheses consistent with the training examples.
 
 import csv
 
